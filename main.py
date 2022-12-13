002c536df71e735bc03b54b89dc69ccfc441ce80
Rob1to <calnahuac@yahoo.com.mx>
2022-12-13 15:28:30 +0100

    robs commit 2

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 def print_congrats():
-<<<<<<< HEAD
-    print('Congrats, you`ve made it!')
-    print('ROB part!')
-=======
-    print('Congrats, you`ve made ijhjgkgkgt!')
->>>>>>> 5bb2d6cdde47af7f1b66669b9d4c89ffcb868aef
+    print('Congrats, you`ve made ijhjgkgkt!')
+    print("robs part")
 
 if __name__ == '__main__':
     print_congrats()
