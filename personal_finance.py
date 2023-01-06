ce056e611f541c0944d142bf029e4fa330c938f8
oahq90 <oahq_90@hotmail.com>
2023-01-07 00:12:24 +0100

    second try

---
+++ personal_finance.py
@@ -0,0 +1,28 @@
+### List of packages that need to be installed ###
+## Task 1 and 3: splitwise, sqlite3, json
+## Task 7: pyqt5, pyqt5-tools
+
+### Importing all the necessary libraries ###
+from first_third_tasks import *
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+# Running Splitwise Sync and currency process (Task 1 and 3) #
+#run_sync_currency()
+
+## Defining the App ##
+def Personal_Finance():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(900,400,700,500)
+    win.setWindowTitle("Personal Finance: A Splitwise Extension (Prototype)")
+
+    label = QtWidgets.QLabel(win)
+    label.setText("Finance App")
+    label.move(300,50)
+
+    win.show()
+    sys.exit(app.exec_())
+
+Personal_Finance()
