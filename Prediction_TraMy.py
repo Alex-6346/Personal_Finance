34bccc39ad5d03b61e8fd6723a8c93ba453e11ae
tramysitz <tramysitz.1999@gmail.com>
2023-02-01 16:29:30 +0100

    'finished predicition, only plot is missing'

--- Prediction_TraMy.py
+++ Prediction_TraMy.py
@@ -2,7 +2,7 @@ from sklearn import linear_model
 import sqlite3
 import numpy as np
 import pandas as pd
-import datetime
+from datetime import datetime
 import math
 
 
@@ -24,6 +24,7 @@ except sqlite3.IntegrityError as err:
 
 sql_query = pd.read_sql_query('''SELECT * FROM Transactions JOIN TransactionItems AS ti ON Transactions.id = ti.transaction_id WHERE user_id = 61730143''', sqliteConnection )
 
+#month = pd.read_sql_query('''SELECT * FROM Transactions AS t INNER JOIN TransactionItems AS ti ON t.id = ti.transaction_id INNER JOIN Subcategories as s ON s.id = t.subcategory_id WHERE expense_date BETWEEN date('now', '-3 months') AND date('now') AND ti.user_id = "61730143"''', sqliteConnect)
 df = pd.DataFrame(sql_query, columns = ['id', 'expense_date', 'group_id', 'subcategory_id', 'description', 'currency_code', 'repeat_interval', 'updated_date', 'id', 'transaction_id', 'user_id', 'amount', 'base_amount'])
 print(df)
 data = df[["expense_date", "subcategory_id", "base_amount"]]
@@ -38,7 +39,8 @@ def timestamp_change(x):
     return y
 
 # Changing timestamp format #
-data["expense_date"].apply(timestamp_change)
+# Gives a warning. Do not panic, it works #
+data["expense_date"] = data["expense_date"].apply(timestamp_change)
 # number of months
 date1 = data["expense_date"][0]
 date2 = data["expense_date"].iloc[-1]
@@ -46,11 +48,49 @@ num_months = math.ceil(((date1-date2)/np.timedelta64(1, 'M')))
 #extracting month value
 pd.DatetimeIndex(data["expense_date"]).month
 
-#Obtaining balances
-balance_feb = 0
-balance_jan = 0
-balance_dec =0
-#for i in [2,1,12] :
-  #  temp = data[data['expense_date'].dt.strftime('%m') == i]
-   # for row in temp:
-        #if ['subcategory_id']
+
+#december
+sum_expenses_dec = pd.read_sql_query('''SELECT expense_date, subcategory_id, sum(base_amount) FROM Transactions JOIN TransactionItems AS ti ON Transactions.id = ti.transaction_id WHERE user_id = 61730143 AND strftime('%m', expense_date) = '12' AND subcategory_id NOT BETWEEN 101 AND 105 GROUP BY subcategory_id''', sqliteConnection )
+sum_income_dec = pd.read_sql_query('''SELECT expense_date, subcategory_id, sum(base_amount) FROM Transactions JOIN TransactionItems AS ti ON Transactions.id = ti.transaction_id WHERE user_id = 61730143 AND strftime('%m', expense_date) = '12' AND subcategory_id BETWEEN 101 AND 105 GROUP BY subcategory_id''', sqliteConnection )
+
+#january
+
+sum_expenses_jan = pd.read_sql_query('''SELECT expense_date, subcategory_id, sum(base_amount) FROM Transactions JOIN TransactionItems AS ti ON Transactions.id = ti.transaction_id WHERE user_id = 61730143 AND strftime('%m', expense_date) = '01' AND subcategory_id NOT BETWEEN 101 AND 105 GROUP BY subcategory_id''', sqliteConnection )
+sum_income_jan = sum_expenses_dec = pd.read_sql_query('''SELECT expense_date, subcategory_id, sum(base_amount) FROM Transactions JOIN TransactionItems AS ti ON Transactions.id = ti.transaction_id WHERE user_id = 61730143 AND strftime('%m', expense_date) = '01' AND subcategory_id BETWEEN 101 AND 105 GROUP BY subcategory_id''', sqliteConnection )
+
+#february
+
+sum_expenses_feb = pd.read_sql_query('''SELECT expense_date, subcategory_id, sum(base_amount) FROM Transactions JOIN TransactionItems AS ti ON Transactions.id = ti.transaction_id WHERE user_id = 61730143 AND strftime('%m', expense_date) = '02' AND subcategory_id NOT BETWEEN 101 AND 105 GROUP BY subcategory_id''', sqliteConnection )
+sum_income_feb = sum_expenses_dec = pd.read_sql_query('''SELECT expense_date, subcategory_id, sum(base_amount) FROM Transactions JOIN TransactionItems AS ti ON Transactions.id = ti.transaction_id WHERE user_id = 61730143 AND strftime('%m', expense_date) = '02' AND subcategory_id BETWEEN 101 AND 105 GROUP BY subcategory_id''', sqliteConnection )
+
+### converting to data frame ###
+exp_dec_df = pd.DataFrame(sum_expenses_dec, columns = ['expense_date', 'subcategory_id', 'sum(base_amount)'])
+inc_dec_df = pd.DataFrame(sum_income_dec, columns = ['expense_date', 'subcategory_id', 'sum(base_amount)'])
+
+exp_jan_df = pd.DataFrame(sum_expenses_jan, columns = ['expense_date', 'subcategory_id', 'sum(base_amount)'])
+inc_jan_df = pd.DataFrame(sum_income_jan, columns = ['expense_date', 'subcategory_id', 'sum(base_amount)'])
+
+exp_feb_df = pd.DataFrame(sum_expenses_feb, columns = ['expense_date', 'subcategory_id', 'sum(base_amount)'])
+inc_feb_df = pd.DataFrame(sum_income_feb, columns = ['expense_date', 'subcategory_id', 'sum(base_amount)'])
+
+##sum expenses and income in each month
+
+total_exp_dec = exp_dec_df['sum(base_amount)'].sum()
+total_inc_dec = inc_dec_df['sum(base_amount)'].sum()
+balance_dec = total_inc_dec-total_exp_dec
+
+total_exp_jan = exp_jan_df['sum(base_amount)'].sum()
+total_inc_jan = inc_jan_df['sum(base_amount)'].sum()
+balance_jan = total_inc_jan-total_exp_jan
+
+total_exp_feb = exp_feb_df['sum(base_amount)'].sum()
+total_inc_feb = inc_feb_df['sum(base_amount)'].sum()
+balance_feb = total_inc_feb-total_exp_feb
+
+### Linear Model ###
+reg = linear_model.LinearRegression()
+Y = [balance_dec,balance_jan,balance_feb]
+reg.fit([[1],[2],[3]],Y)
+reg.coef_
+reg.intercept_
+reg.predict([[4]])
--- reporting_TraMy.py
+++ reporting_TraMy.py
@@ -1,5 +1,6 @@
 import sqlite3
 import pandas as pd
+import matplotlib
 
 try:
     sqliteConnect = sqlite3.connect("61730143.sqlite")
@@ -17,7 +18,7 @@ try:
             ON t.id = ti.transaction_id
         INNER JOIN Subcategories as s
             ON s.id = t.subcategory_id
-        WHERE ti.user_id = "61730143";
+        WHERE ti.user_id = "61730143" AND ;
         
     '''
     #since we only add data from december 2022 you need to set up in settings at least march 2023 to get all data
@@ -25,12 +26,16 @@ try:
     month1 = pd.read_sql_query('''SELECT * FROM Transactions AS t INNER JOIN TransactionItems AS ti ON t.id = ti.transaction_id INNER JOIN Subcategories as s ON s.id = t.subcategory_id WHERE expense_date BETWEEN date('now', '-1 months') AND date('now') AND ti.user_id = "61730143"''', sqliteConnect)
     month2 = pd.read_sql_query('''SELECT * FROM Transactions AS t INNER JOIN TransactionItems AS ti ON t.id = ti.transaction_id INNER JOIN Subcategories as s ON s.id = t.subcategory_id WHERE expense_date BETWEEN date('now', '-2 months') AND date('now', '-1 months') AND ti.user_id = "61730143"''', sqliteConnect)
     month3 = pd.read_sql_query('''SELECT * FROM Transactions AS t INNER JOIN TransactionItems AS ti ON t.id = ti.transaction_id INNER JOIN Subcategories as s ON s.id = t.subcategory_id WHERE expense_date BETWEEN date('now', '-3 months') AND date('now', '-2 months') AND ti.user_id = "61730143"''', sqliteConnect)
-    print(month2)
+    repeat = pd.read_sql_query('''SELECT * FROM Transactions AS t INNER JOIN TransactionItems AS ti ON t.id = ti.transaction_id INNER JOIN Subcategories as s ON s.id = t.subcategory_id WHERE repeat_interval = "monthly" AND ti.user_id = "61730143"''', sqliteConnect)
+    expenses = pd.read_sql_query('''SELECT * FROM Transactions AS t INNER JOIN TransactionItems AS ti ON t.id = ti.transaction_id INNER JOIN Subcategories as s ON s.id = t.subcategory_id WHERE category_id NOT LIKE '100' AND ti.user_id = "61730143"''', sqliteConnect)
+    income = pd.read_sql_query('''SELECT * FROM Transactions AS t INNER JOIN TransactionItems AS ti ON t.id = ti.transaction_id INNER JOIN Subcategories as s ON s.id = t.subcategory_id WHERE category_id LIKE '100' AND ti.user_id = "61730143"''', sqliteConnect)
+    print(income)
 except sqlite3.IntegrityError as err:
     print(err)
 
 
-
+#need to get rid off salary
+#need to seperate expenses with repeat interval
 
 
 
